2a729c5471d0f08524192e3aabefc4259fe29205
hms1759 <toheeb.black@gmail.com>
2023-08-29 09:22:06 +0100

    saved

--- CoachFrika/Controllers/HomeController.cs
+++ CoachFrika/Controllers/HomeController.cs
@@ -1,12 +1,16 @@
 ﻿using CoachFrika.Extensions;
+using CoachFrika.GoogleExtension;
 using CoachFrika.Models;
 using CoachFrika.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System.Diagnostics;
 using System.Net.Http.Json;
+using static Google.Apis.Sheets.v4.SpreadsheetsResource.ValuesResource;
 
 namespace CoachFrika.Controllers
 {
@@ -16,13 +20,18 @@ namespace CoachFrika.Controllers
         public readonly IConfiguration configuration;
         public readonly IEmailService _emailService;
         private readonly EmailConfigSettings _emailConfig;
-        private HttpClient m_Client;
-        public HomeController(ILogger<HomeController> logger, IEmailService emailService, IOptions<EmailConfigSettings> emailConfig)
+        private HttpClient m_Client; 
+        const string SPREADSHEET_ID = "1RrG88SEXb5p85PzQUXhfzccHDa97hdAXyeueoqnUbSk";
+        const string SHEET_NAME = "Sheets";
+        SpreadsheetsResource.ValuesResource _googleSheetValues;
+
+        public HomeController(ILogger<HomeController> logger, GoogleSheetsHelper googleSheetsHelper, IEmailService emailService, IOptions<EmailConfigSettings> emailConfig)
         {
             _logger = logger;
             _emailConfig = emailConfig.Value;
             _emailService = emailService;
             m_Client = new HttpClient();
+            _googleSheetValues = googleSheetsHelper.Service.Spreadsheets.Values;
         }
 
         public IActionResult Index()
@@ -37,6 +46,15 @@ namespace CoachFrika.Controllers
 
                 return RedirectToAction("Index");
             }
+            var range = "Sheet2A1:D5";
+            var valueRange = new ValueRange
+            {
+                Values = GoogleMapper.MapToRangeContactData(model)
+            };
+
+            var appendRequest = _googleSheetValues.Append(valueRange, SPREADSHEET_ID, range);
+            appendRequest.ValueInputOption = AppendRequest.ValueInputOptionEnum.USERENTERED;
+            appendRequest.Execute();
             // sending email
             var mailSubject = _emailConfig.ContactTopic;
             var mailto = _emailConfig.MailTo.ToList();
@@ -65,16 +83,16 @@ namespace CoachFrika.Controllers
         [HttpPost]
         public async Task<IActionResult> RequestPlan(ContactUs model)
         {
+            var range = "Sheet1!A1:D5";
+            var valueRange = new ValueRange
+            {
+                Values = GoogleMapper.MapToRangeData(model)
+            };
 
-            //HttpClient httpClient = new HttpClient();
-            //string baseUrl = " https://sheetdb.io/api/v1/v0l3ssbj152qu";
+            var appendRequest = _googleSheetValues.Append(valueRange, SPREADSHEET_ID, range);
+            appendRequest.ValueInputOption = AppendRequest.ValueInputOptionEnum.USERENTERED;
+            appendRequest.Execute();
 
-            //HttpResponseMessage response =await  httpClient.PostAsJsonAsync(baseUrl,model);
-
-            //if (response.IsSuccessStatusCode)
-            //{
-            //    string stateInfo = response.Content.ReadAsStringAsync().Result;
-            //}
             // sending email
             var mailSubject = _emailConfig.RequestTopic;
             var body = await _emailService.ReadTemplate("planRequest");
