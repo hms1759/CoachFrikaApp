0fff4b0fab1d34a7d5adfdf8f12f76526ec55ad2
Isola Opeyemi <Isola.Opeyemi@thegiggroupng.com>
2024-11-30 15:01:24 +0100

    update migration

---
+++ CoachFrika/Migrations/20241130135540_update.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoachFrika.Migrations
+{
+    public partial class update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Teachers_AspNetUsers_IdentityUserId1",
+                table: "Teachers");
+
+            migrationBuilder.DropTable(
+                name: "Batches");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Teachers_IdentityUserId1",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityUserId",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityUserId1",
+                table: "Teachers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "IdentityUserId",
+                table: "Teachers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityUserId1",
+                table: "Teachers",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Batches",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TeachersId1 = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeletedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TeachersId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Batches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Batches_AspNetUsers_TeachersId1",
+                        column: x => x.TeachersId1,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teachers_IdentityUserId1",
+                table: "Teachers",
+                column: "IdentityUserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Batches_TeachersId1",
+                table: "Batches",
+                column: "TeachersId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teachers_AspNetUsers_IdentityUserId1",
+                table: "Teachers",
+                column: "IdentityUserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
