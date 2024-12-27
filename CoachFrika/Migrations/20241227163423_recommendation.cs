950f2c26adca7752a22e97d120d480c6b86e12ec
Toheeb Ishola <TIshola4@polarisbanklimited.com>
2024-12-27 18:20:04 +0100

    migration fixes

---
+++ CoachFrika/Migrations/20241227163423_recommendation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoachFrika.Migrations
+{
+    public partial class recommendation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recommendations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Recommendation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CoachId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TeacherId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ScheduleId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ScheduleId1 = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    TeacherRemark = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recommendations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Recommendations_Schedule_ScheduleId1",
+                        column: x => x.ScheduleId1,
+                        principalTable: "Schedule",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recommendations_ScheduleId1",
+                table: "Recommendations",
+                column: "ScheduleId1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recommendations");
+        }
+    }
+}
