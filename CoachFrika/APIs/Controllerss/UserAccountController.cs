f647f9cce18ad30eeae3dedff31326195f7bc549
Isola Opeyemi <Isola.Opeyemi@thegiggroupng.com>
2025-01-04 17:48:43 +0100

    update fixes

--- CoachFrika/APIs/Controllerss/UserAccountController.cs
+++ CoachFrika/APIs/Controllerss/UserAccountController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CoachFrika.APIs.Controllerss
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UserAccountController : ControllerBase
